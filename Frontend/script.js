365d63b7de5bdd1dcffa6a6c6cd5474cbde14ef9
Pushpak192 <digambarzambare1101@gmail.com>
2026-04-03 16:12:30 +0530

    Update script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,5 +1,5 @@
 function getMessage() {
-  fetch("http://backend:3000/api/message")
+  fetch("/api/message")
     .then(res => res.json())
     .then(data => {
       document.getElementById("output").innerText = data.message;
